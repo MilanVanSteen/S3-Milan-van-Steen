07b26f7912f1564b751005d903f6903b902328f1
MilanVanSteen <537674@student.fontys.nl>
2025-05-19 11:22:24 +0200

    Added CategroyContainer

---
+++ back-end/src/main/java/com/example/backend/Containers/CategoryContainer.java
@@ -0,0 +1,38 @@
+package com.example.backend.Containers;
+
+import com.example.backend.DTOs.CategoryDTO;
+import com.example.backend.Interfaces.CategoryInterface;
+import com.example.backend.Mappers.CategoryMapper;
+import com.example.backend.Models.Category;
+
+import java.util.List;
+
+public class CategoryContainer {
+    private final CategoryInterface repo;
+
+    public CategoryContainer(CategoryInterface _repo) {
+        this.repo = _repo;
+    }
+
+    public List<Category> getAllCategories() {
+        return CategoryMapper.toModelList((List<CategoryDTO>)repo.findAll());
+    }
+
+    public Category getCategoryById(int categoryID) {
+        return CategoryMapper.toModel(repo.findById(categoryID).orElse(null));
+    }
+
+
+    public Category CreateCategory(Category category){
+        return CategoryMapper.toModel(repo.save(CategoryMapper.toDTO(category)));
+    }
+
+    public boolean DeleteCategory(int categoryID){
+        try {
+            repo.deleteById(categoryID);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
--- back-end/src/main/java/com/example/backend/Mappers/CategoryMapper.java
+++ back-end/src/main/java/com/example/backend/Mappers/CategoryMapper.java
@@ -3,6 +3,9 @@ package com.example.backend.Mappers;
 import com.example.backend.DTOs.CategoryDTO;
 import com.example.backend.Models.Category;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryMapper {
     public static CategoryDTO toDTO(Category category) {
         if(category == null) {
@@ -16,5 +19,23 @@ public class CategoryMapper {
             return null;
         }
         return new Category(categoryDTO.getCategoryID(), categoryDTO.getName());
+    }
+
+    public static List<CategoryDTO> toDTOList(List<Category> categories) {
+        if (categories == null) {
+            return new ArrayList<>();
+        }
+        return categories.stream()
+                .map(CategoryMapper::toDTO)
+                .toList();
+    }
+
+    public static List<Category> toModelList(List<CategoryDTO> categoryDTOs) {
+        if (categoryDTOs == null) {
+            return new ArrayList<>();
+        }
+        return categoryDTOs.stream()
+                .map(CategoryMapper::toModel)
+                .toList();
     }
 }
